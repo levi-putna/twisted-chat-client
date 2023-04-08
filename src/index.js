0e6360bd1df92491b5a903c54674460035dd4a3b
Levi Putna <levi.putna@gmail.com>
2023-04-08 20:11:00 +1000

    Update DOM load event to use page load event

--- src/index.js
+++ src/index.js
@@ -4,22 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const rootDiv = document.createElement('div');
-  rootDiv.setAttribute('id', 'chat-bot-client');
+window.onload = () => {
+    const rootDiv = document.createElement('div');
+    rootDiv.setAttribute('id', 'chat-bot-client');
 
-  const body = document.getElementsByTagName('body')[0];
-  body.appendChild(rootDiv);
+    const body = document.getElementsByTagName('body')[0];
+    body.appendChild(rootDiv);
 
-  const root = ReactDOM.createRoot(rootDiv);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
+    const root = ReactDOM.createRoot(rootDiv);
+    root.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
 
-  // If you want to start measuring performance in your app, pass a function
-  // to log results (for example: reportWebVitals(console.log))
-  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-  reportWebVitals();
-});
+    // If you want to start measuring performance in your app, pass a function
+    // to log results (for example: reportWebVitals(console.log))
+    // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+    reportWebVitals();
+};
